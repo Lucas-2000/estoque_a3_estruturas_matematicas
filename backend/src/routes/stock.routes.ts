4ab4269485461e8015658ab10cf28fa6d2f8e5c9
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-03-22 15:52:02 -0300

    adicionado retorno do estoque

--- backend/src/routes/stock.routes.ts
+++ backend/src/routes/stock.routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { InMemoryStockRepository } from "./../repositories/in-memory/in-memory-stock-repository";
 import { addItemFactory } from "../use-cases/stock/add-item/add-item-factory";
 import { itemRepository } from "./item.routes";
+import { showStockFactory } from "../use-cases/stock/show/show-stock-factory";
 
 const stockRoutes = Router();
 
@@ -9,6 +10,10 @@ const stockRepository = new InMemoryStockRepository();
 
 stockRoutes.post("/", (req: Request, res: Response) => {
   return addItemFactory(stockRepository, itemRepository).handle(req, res);
+});
+
+stockRoutes.get("/", (req: Request, res: Response) => {
+  return showStockFactory(stockRepository).handle(req, res);
 });
 
 export { stockRoutes };
